50e3e164298ea2ed43d2c0d23df00d9ecf8de30e
Umit KARABACAK <umit.karabacak@hotmail.com.tr>
2020-03-27 17:28:01 +0300

    Add Data Layer -Database Folder for contexts -MappingFolder for fluent APIs

---
+++ ArticleManagement.Data/Mappings/CommentMappings.cs
@@ -0,0 +1,15 @@
+﻿using ArticleManagement.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ArticleManagement.Data.Mappings
+{
+    public class CommentMappings : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("Comments");
+        }
+
+    }
+}
